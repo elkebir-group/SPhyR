87843796c720869db09fd192ce071b5789480b6a
Mohammed El-Kebir <melkebir@cs.brown.edu>
2018-03-20 18:30:38 -0500

    new kDollo instances

--- data/k_dollo/generate.sh
+++ data/k_dollo/generate.sh
@@ -7,13 +7,13 @@ fi
 
 for n in {25,50,100,250}
 do
-    for m in {50,100}
+    for m in {25,50,100,250}
     do
         for s in {1..20}
         do
             for loss in {0.1,0.2,0.4}
             do
-                for k in {1,2}
+                for k in {1,2,3}
                 do
                     filename=m${m}_n${n}_s${s}_k${k}_loss${loss}
                     $1 -loss $loss -k $k -s $s -A $filename.A -B $filename.B -dot $filename.dot ../perfect_phylogeny/m${m}_n${n}_s${s}.txt
